6b587a19e218aefb018cdd07fefe853b47599ea8
Moya11706 <jsir11708@gmail.com>
2026-05-09 18:04:30 -0500

    Create Entidad_Base.py

---
+++ Entidad_Base.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from uuid import uuid4
+
+
+class Entidad(ABC):
+    def __init__(self, codigo=None):
+        if codigo is None or not str(codigo).strip():
+            self._codigo = uuid4().hex[:8].upper()
+        else:
+            self._codigo = str(codigo).strip()
+
+    
+    def codigo(self):
+        return self._codigo
+
+ 
+    def validar_texto(valor, campo):
+        if not isinstance(valor, str) or not valor.strip():
+            raise ValueError(f"El campo '{campo}' no puede estar vacío.")
+        return valor.strip()
+
+    @abstractmethod
+    def mostrar_info(self):
+        pass
